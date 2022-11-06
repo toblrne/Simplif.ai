dbaef6d471359cce5435b0b4eac1834bfea9856a
Allan Tan <allan@dhcp-128-189-72-108.ubcsecure.wireless.ubc.ca>
2022-11-06 14:41:59 -0800

    linked fb

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,23 @@ import { collection, getDocs } from "@firebase/firestore"
 
 function App() {
 
+  useEffect(() => {
+    const getSummary = (fullText) => {
+      fetch("http://localhost:5000/getdata"                                               )
+      .then((res) => {
+        return res.text()
+      })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error.status);
+      })
+    }
+    getSummary("asdf");
+  })
+  
+
   return (
     <div className="App">
       
